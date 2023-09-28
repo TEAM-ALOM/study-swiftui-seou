c292a9d6b78d9e9dfe65ad2e0c1f119ea35a2b94
SeoU <tjdnfkd1225@naver.com>
2023-09-28 21:41:19 +0900

    Create app

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 은서우 on 2023/09/28.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
